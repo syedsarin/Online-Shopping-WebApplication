4cdb5cc99ee1b6f425b69293d5c2cf8b0fb3d037
syedsarin <146479866+syedsarin@users.noreply.github.com>
2025-07-14 18:10:21 +0530

    ICategoryRepository.java

--- src/main/java/com/ssm/repository/ICategoryRepository.java
+++ src/main/java/com/ssm/repository/ICategoryRepository.java
@@ -1,6 +1,10 @@
 package com.ssm.repository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
