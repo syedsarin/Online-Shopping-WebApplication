7987664d40315bd2215eebe932ebb81314b07e25
syedsarin <146479866+syedsarin@users.noreply.github.com>
2025-07-14 17:56:20 +0530

    ICartRepository.java

--- src/main/java/com/ssm/repository/ICartRepository.java
+++ src/main/java/com/ssm/repository/ICartRepository.java
@@ -1,4 +1,8 @@
 package com.ssm.repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
